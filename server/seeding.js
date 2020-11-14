c2d431564d8fe014ce9eac2c965ac64dd209a207
Edgar Cetino <cetino-e@hotmail.com>
2020-11-14 15:28:15 -0800

    changed seeding script, installed test suite, added react component to display photos

--- server/seeding.js
+++ server/seeding.js
@@ -1,27 +1,28 @@
-const faker = require('faker');
 const { ItemImages } = require('../database/index.js');
 
-var seeder = () => {
-  var fiveImageUrls = [];
-
-  for (var i = 0; i < 5; i++) {
-    var oneImageUrl = faker.image.fashion();
-    fiveImageUrls.push(oneImageUrl);
-  }
-
-  for (var i = 1; i < 101; i++) {
+const seeder = () => {
+  let lastSeed = 1;
+  let lastIncrement = 6;
+  for (let i = 1; i < 101; i++) {
+    const fiveImageUrls = [];
+    for (let j = lastSeed; j < lastIncrement; j++) {
+      const oneImageUrl = `http://picsum.photos/seed/${j}/840/840`;
+      fiveImageUrls.push(oneImageUrl);
+    }
     const oneSeed = new ItemImages({
       id: i,
-      imageUrls: fiveImageUrls
-    })
+      imageUrls: fiveImageUrls,
+    });
     oneSeed.save((err) => {
-      if(err) {
-        console.log('err saving', err)
+      if (err) {
+        console.log('err saving', err);
       } else {
-        console.log('succesfully saved into db')
+        console.log('succesfully saved into db');
       }
-    })
+    });
+    lastSeed += 5;
+    lastIncrement += 5;
   }
-}
+};
 
 seeder();
